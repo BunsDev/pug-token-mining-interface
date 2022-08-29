dbbc15546665bf09ff82c036d4068dd8209e3681
PugswapFinance <111673538+PugswapFinance@users.noreply.github.com>
2022-08-29 18:23:31 +0700

    Update pools.ts

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -1,7 +1,7 @@
 export const Pools = [
     {
         poolAddresses: {
-            97: '0x1D8249b17ced93b7bbA015AAc5fC4B13AE76fcc0',
+            97: '0x6fF5974f035E7d3888D78848Eb7C66dA0Ba1f65e',
         },
         stakingTokenAddresses: {
             97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
@@ -29,10 +29,10 @@ export const Pools = [
     },
     {
         poolAddresses: {
-            97: '0xd2a5C2D607513485475F0CD62f69c01E014983E5',
+            97: '0x2dF79718211a5CC49C512f950F556b2617e6754E',
         },
         stakingTokenAddresses: {
-            97: '0x6ce8dA28E2f864420840cF74474eFf5fD80E65B8',
+            97: '0x578818aB780a0B41C5D809370B77d9246bA6cB0d',
         },
         name: 'BTCB',
         symbol: 'BTCB',
@@ -40,79 +40,6 @@ export const Pools = [
         isWBNB: false,
         icon: 'btcb',
         magnification: 1,
-    },
-    {
-        poolAddresses: {
-            97: '0x169795A1D4cdb0e84c395a8Fd35D583176469679',
-        },
-        stakingTokenAddresses: {
-            97: '0x337610d27c682E347C9cD60BD4b3b107C9d34dDd',
-        },
-        name: 'USDT',
-        symbol: 'USDT',
-        tokenSymbol: 'USDT',
-        isWBNB: false,
-        icon: 'usdt',
-        magnification: 1,
-    },
-    {
-        poolAddresses: {
-            97: '0xB9eEe48F597c551843FcC099f90013895B9c7263',
-        },
-        stakingTokenAddresses: {
-            97: '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee',
-        },
-        acceleratorAddresses: {
-            97: '0x7377ED43F5A79cc89A5cE5D6632764a3494996B7',
-        },
-        name: 'BUSD',
-        symbol: 'BUSD',
-        tokenSymbol: 'BUSD',
-        isWBNB: false,
-        icon: 'busd',
-        nftSymbol: 'TEST',
-        magnification: 1,
-    },
-    {
-        poolAddresses: {
-            97: '0x1D9AD0412880EC34545B1dC066a9f662Ec843F1E',
-        },
-        stakingTokenAddresses: {
-            97: '0x2C1E720417dED34cf5ae648de344379E679DFa51',
-        },
-        name: 'BUSD/BNB',
-        symbol: 'BUSD/BNB',
-        tokenSymbol: 'BUSD/BNB',
-        isWBNB: false,
-        icon: 'bnb-busd',
-        magnification: 2,
-    },
-    {
-        poolAddresses: {
-            97: '0x9d7b39511dcf270a0c6eE555D6e190e72E40cbB1',
-        },
-        stakingTokenAddresses: {
-            97: '0x60A8C5CEB93a453731587d9463657e1c659A8F07',
-        },
-        name: 'ETH/BNB',
-        symbol: 'ETH/BNB',
-        tokenSymbol: 'ETH/BNB',
-        isWBNB: false,
-        icon: 'eth-busd',
-        magnification: 2,
-    },
-    {
-        poolAddresses: {
-            97: '0x0a6eC00cB7E1967066fecdD1de158A386Fd40A17',
-        },
-        stakingTokenAddresses: {
-            97: '0xE19F1D8Bf1186ac4648122fFdd430CD65972af48',
-        },
-        name: 'USDT/BNB',
-        symbol: 'USDT/BNB',
-        tokenSymbol: 'USDT/BNB',
-        isWBNB: false,
-        icon: 'usdt-busd',
-        magnification: 2,
+
     },
 ]
