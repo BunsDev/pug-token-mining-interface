5bad270f7c8c110be347548d6dbec0d80c39a0aa
PugswapFinance <111673538+PugswapFinance@users.noreply.github.com>
2022-08-29 19:02:11 +0700

    Update pools.ts

--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -1,7 +1,7 @@
 export const Pools = [
     {
         poolAddresses: {
-            97: '0x6fF5974f035E7d3888D78848Eb7C66dA0Ba1f65e',
+            97: '0xD6d89Aa2E448c32A39B09BB9e7275900FA31A1eC',
         },
         stakingTokenAddresses: {
             97: '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd',
@@ -15,28 +15,28 @@ export const Pools = [
     },
     {
         poolAddresses: {
-            97: '0x95630780b40A0e2617749BC25D4aA920D3Ba0463',
+            97: '0x883A8A6c7FF1907720040f59eF09cF08Ba9265b7',
         },
         stakingTokenAddresses: {
-            97: '0xd66c6B4F0be8CE5b39D52E0Fd1344c389929B378',
+            97: '0xeD24FC36d5Ee211Ea25A80239Fb8C4Cfd80f12Ee',
         },
-        name: 'ETH',
-        symbol: 'ETH',
-        tokenSymbol: 'ETH',
+        name: 'BUSD',
+        symbol: 'BUSD',
+        tokenSymbol: 'BUSD',
         isWBNB: false,
         icon: 'eth',
         magnification: 1,
     },
     {
         poolAddresses: {
-            97: '0x2dF79718211a5CC49C512f950F556b2617e6754E',
+            97: '0x16b711bC75bcb6c9DeAFe0CdB550bE2401eEAF3D',
         },
         stakingTokenAddresses: {
-            97: '0x578818aB780a0B41C5D809370B77d9246bA6cB0d',
+            97: '0x5B87A076E6CdFd043FCc8d8052FB8EA5E49580cC',
         },
-        name: 'BTCB',
-        symbol: 'BTCB',
-        tokenSymbol: 'BTCB',
+        name: 'PUG',
+        symbol: 'PUG',
+        tokenSymbol: 'PUG',
         isWBNB: false,
         icon: 'btcb',
         magnification: 1,
